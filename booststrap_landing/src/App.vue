f1b1a41887d324a3f581982933b29b71ce74e981
AssaBaradji <baradjiassa695@gmail.com>
2024-08-10 14:36:34 +0200

    first-commit

---
+++ booststrap_landing/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="app">
+      <Navbar />
+      <Jumbotron />
+    </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import Jumbotron from './components/Jumbotron.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Jumbotron,
+  }
+};
+</script>
+
+<style>
+#app{
+  font-family:Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing:grayscale;
+}
+  
+</style>
---
+++ booststrap_landing/src/components/Jumbotron.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="contenair my-5">
+        <div class="row">
+            <div class="col-md-7"><img src="https://picsum.photos/id/237/600/400" alt=""></div>
+            <div class="col-md-5">
+                <h1 class="font-weight-light">Tagline</h1>
+                <p class="mt-4">
+                    This is a template that is great for small business.it doesn't 
+                    have two much ancy flare to it, but it makes a great use of the standarrd 
+                    Bootstrap core components. Feel free to use this template for any 
+                    project you want!
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Jumbotron",
+    };
+</script>
+
+<style>
+
+</style>

1f65535dc90eab7e49d44f17f4c7814db4f3e79a
AssaBaradji <baradjiassa695@gmail.com>
2024-08-11 13:45:56 +0200

    second-commmit

--- booststrap_landing/src/App.vue
+++ booststrap_landing/src/App.vue
@@ -2,18 +2,27 @@
     <div class="app">
       <Navbar />
       <Jumbotron />
+      <CTA />
+      <Cards />
+      <Footer />
     </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
 import Jumbotron from './components/Jumbotron.vue'
+import CTA from './components/CTA.vue'
+import Cards from './components/Cards.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
     Navbar,
     Jumbotron,
+    CTA,
+    Cards,
+    Footer,
   }
 };
 </script>
---
+++ booststrap_landing/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="footer py-3 bg-light mt-5 bg-dark">
+        <div class="container">
+            <span class="text-white">Place sticky footer content here.</span>
+        </div>
+</footer>
+</template>
+
+<script>
+    export default {
+        name:"Footer",
+    };
+</script>
+
+<style>
+
+</style>
